31d29a20d9e4b2e774d3e836f87b2c398d1d819e
Aman Minz <220107384+AmanMInz-in@users.noreply.github.com>
2026-02-10 21:39:24 +0530

    script for slicing and printing items from a list

---
+++ Exercies/slices.py
@@ -0,0 +1,13 @@
+list=["pizza","burger","fries","icecream","coke","samosa","biryani"]
+print("printing first three items from the list")
+print(list[0:3])
+
+print("\n printing first three iteme using slicing \n")
+print(list[:3])
+
+print("\n printing items from middle of the list")
+print(list[3:6])
+
+print("\n the last 3 items from the list are")
+
+print(list[-3:])
