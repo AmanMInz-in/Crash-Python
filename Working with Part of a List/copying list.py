c2cdb4f1cb599d4e81e266b4798bc363c0b179f7
Aman Minz <220107384+AmanMInz-in@users.noreply.github.com>
2026-02-10 18:16:08 +0530

    Add scripts for copying lists and looping through tuple values

---
+++ Working with Part of a List/copying list.py
@@ -0,0 +1,10 @@
+myfood = ["pizza", "burger", "pasta", "salad"]
+friendfood = myfood[:]
+print("my favorite food is : ")
+for i in myfood:
+    print(i.title())
+
+print("\n my firend's favorite food is :")
+for i in friendfood:
+    print(i.title())
+      
---
+++ Working with Part of a List/looping through all values in tuple.py
@@ -0,0 +1,8 @@
+value=[100,50]
+for values in value:
+    print(values)
+
+print("\n modifying the tuple values \n")
+value=(200,300)
+for values in value:
+    print(values)
