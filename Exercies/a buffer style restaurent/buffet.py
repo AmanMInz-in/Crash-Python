3f6c1795c31e09aaaf2d4ef4e74ecf021c6be558
Aman Minz <220107384+AmanMInz-in@users.noreply.github.com>
2026-02-13 00:59:28 +0530

    writing over tupple

---
+++ Exercies/a buffer style restaurent/buffet.py
@@ -0,0 +1,10 @@
+buffet=("Rajma Chawal","Daal Chawal","Aloo Paratha","Chole Bhature","Pav Bhaji")
+print("\n printing the value one by one by looping through the list: ")
+for food in buffet:
+    print("food: "+food)
+
+print("\n printing the modified value one by one by looping through the list:\n")
+
+buffet=("Rajma Chawal With Butter","Daal Chawal","Aloo Paratha","Chole Bhature","Pav Bhaji")
+for food in buffet:
+    print("food: "+food)
---
+++ chapter2_list/Organizing list/writing over tupple.py
@@ -0,0 +1,7 @@
+dimensions=[200,50]
+for dimension in dimensions:
+    print(dimension)
+
+dimensions=(300,400)
+for dimension in dimensions:
+    print(dimensions)
